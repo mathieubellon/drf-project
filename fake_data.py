0606b599e00b2a8f2a5103065913f62abfcea213
Mathieu <contact@mathieubellon.fr>
2024-04-03 10:59:53 +0200

    Create fake data

---
+++ fake_data.py
@@ -0,0 +1,32 @@
+def main():
+    fake: Faker = Faker()
+
+    for i in range(30):
+        idea = Idea.objects.create(
+            name=fake.paragraph(nb_sentences=1),
+            content=fake.text(),
+        )
+        print(f"Created todo. Name: {idea.name}  Content: {idea.content}")
+
+    idea_count = Idea.objects.count()
+
+    print(f"There are {idea_count} ideas in the database")
+
+
+if __name__ == "__main__":
+    import os
+
+    from django.core.wsgi import get_wsgi_application
+
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "decide.settings")
+    application = get_wsgi_application()
+
+    import random
+
+    from faker import Faker
+    from ideas.models import Idea
+
+    main()
+
+
+    
