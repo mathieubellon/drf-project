13028c7f69a0b83ce6a0e3d57905da3c95383d15
Mathieu <contact@mathieubellon.fr>
2024-04-18 13:31:04 +0200

    Update ALLOWED_HOSTS in decide/settings.py

--- decide/settings.py
+++ decide/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["decide-x6lq.onrender.com", "127.0.0.1"]
+ALLOWED_HOSTS = ["decide-czrc.onrender.com", "127.0.0.1"]
 
 
 # Application definition
